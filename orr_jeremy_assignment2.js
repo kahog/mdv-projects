f59cb982359c8e5c69ff043fad9f330088ef1fba
Jeremy Orr <kahog@me.com>
2012-08-06 21:11:59 -0500

    Simple if/else/w/nested/if/else
    if/else template to carry lasts weeks lesson into this week.

--- orr_jeremy_assignment2.js
+++ orr_jeremy_assignment2.js
@@ -1,1 +1,13 @@
-alert("JavaScript works!");
+alert("JavaScript works!");var 
+
+if () {
+		if () {
+		console.log ();
+		} else { ();
+		};
+} else {
+		if () {
+		console.log();
+		} else {();
+		};
+};
