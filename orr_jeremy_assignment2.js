e9a725856f058b6f82b63cf1bf27c21e51cb4fb7
Jeremy Orr <kahog@me.com>
2012-08-10 11:35:10 -0500

    Changed Console Log calls to use returned values
    Corrected the return value var setters

--- orr_jeremy_assignment2.js
+++ orr_jeremy_assignment2.js
@@ -9,46 +9,53 @@ var businessType = "website"
 	steps = ["decided on a website name", "formulated budget", "made business plan", "gathered customers"]
 	sites = ["facebook", "GooglePlus", "twitter", "linkedIn", "pinterest"],
 	likes = [2800, 2400, 1800, 1700, 1400]; 
-	websiteName = "Launchshares.com "
+//	websiteName = "Launchshares.com "
 //Procedure Function 	
 var startABusiness = function(businessType){	
-	var stepCountNumber = steps.length,
-		stepNumber = 0;
-	console.log("We started with " + stepCountNumber + " things to do.");
+	if (businessType === "website"){	
+	console.log("We have " + steps.length + " things to do.");
+	} else {
+	console.log("A website would have been way easier");
+	};
+	
 };//ends the functions 
 //Boolean function (decide a website name) 
 var getWebsiteName = function(LaunchShares, FinallyLaunched){
-	if (LaunchShares === "available"){
+	var websiteName;
+	if (LaunchShares = true){
 	var websiteName = "LaunchShares.com";
 	console.log("The website we are going to build is " + websiteName + ", now we only have 3 things to do");
 	return websiteName;
 	} else {
 	var websiteName = "FinallyLaunched.com";
-	console.log("Even though we preferred LaunchShares.com The website we are going to build is " + websiteName + ", now we only have " + stepCountNumber + "things to do."); 
-	}
+	console.log("Even though we preferred LaunchShares.com The website we are going to build is " + websiteName + ", now we only have " + steps.length + "things to do."); 
+	}return websiteName;
 };
 //Number Function (formulated budget)
 var formulateBudget = function(budget){
 	var monthlyExpense = 1640,
 		monthsTilBroke = budget/monthlyExpense ;
 	while(budget >= monthlyExpense && monthsTilBroke > 0){
-	console.log("We still have " + monthsTilBroke + " months until we are broke.");
+	console.log("We still have " + monthsTilBroke + " months until we are broke. ");
 	monthsTilBroke--;}
+	return monthsTilBroke;
 	};		
 //String Function (make business plan)
 var businessPlan = function(websiteName, businessType){
-	var say = "Glad to have that stuff done for the ";
-	console.log(say + websiteName +businessType)};
+	var say = "Glad to have that stuff done for the " + websiteName + " " + businessType + ".";
+	console.log(say)};
 //Array Function  (gather customers)
 var gatherCustomers = function(likes, sites){
+	var adSitesOn;
 	for (var adSites = 0; adSites < sites.length; adSites++){
 	var adSitesOn = adSites + 1;	
 	console.log("We just joined " +sites[adSites] +  "and have " +likes[adSites] + " from them.  We are now on " +adSitesOn +" sites.")
-	}
-};		
-startABusiness(businessType);
-getWebsiteName("available", "taken");
-formulateBudget(10000);
-businessPlan(websiteName, businessType);
-gatherCustomers(likes, sites);
+	}return adSitesOn;
+};
+
+var websiteName = getWebsiteName(true, false)
+var monthsTilBroke = formulateBudget(budget)
+console.log("We need to start generating revenues");
+var say = businessPlan(websiteName, businessType)
+var adSitesOn = gatherCustomers(likes, sites)
 console.log("Finally we have over 10,000 likes");
