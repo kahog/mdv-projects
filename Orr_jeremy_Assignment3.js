29397aa7f5496137982e7679ee65a3887c812216
Jeremy Orr <kahog@me.com>
2012-08-12 18:08:35 -0500

    fixed meal options

--- Orr_jeremy_Assignment3.js
+++ Orr_jeremy_Assignment3.js
@@ -12,19 +12,20 @@ var isHungry = true;
     	meat:       ["sausage", "ham", "steak", "bacon" ],
     	drink:      ["milk", "orange juice", "coffee"]
 },
-	breakfast {
-    	appetizer:   "bagel",
-    	condiments:  ["cream cheese", "ketchup", "syrup"]     
-    	entree:   	 "eggs"			
-    	meat:       ["sausage", "ham", "steak", "bacon" ],
-    	drink:      ["milk", "orange juice", "coffee"]
+	lunch {
+    	appetizer:   "spinach and artichoke dip",
+    	condiments:  ["ketchup", "bbq sauce", "ranch dressing"]     
+    	entree:   	 "fried potatoes"			
+    	meat:       ["chicken strips", "ham", "steak"],
+    	drink:      ["Mountain Dew", "Ice Tea", "Lemonade"]
 },
-	breakfast {
-    	appetizer:   "bagel",
-    	condiments:  ["cream cheese", "ketchup", "syrup"]     
-    	entree:   	 "eggs"			
-    	meat:       ["sausage", "ham", "steak", "bacon" ],
     	drink:      ["milk", "orange juice", "coffee"]
+	dinner {
+    	appetizer:   "spinach and artichoke dip",
+    	condiments:  ["steak sauce", "ketchup", "salsa"]     
+    	entree:   	 "mashed potatoes"			
+    	meat:       [" baked chicken", "ham", "steak", "shrimp" ],
+    	drink:      ["Dr. Pepper", "Ice tea", "Wine"]
 };
 	
 	
