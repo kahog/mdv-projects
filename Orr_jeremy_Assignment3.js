c78a52e5ccc4c0fa0f35cfc73415432eeaa6e2cf
Jeremy Orr <kahog@me.com>
2012-08-12 16:08:29 -0500

    added some console logs and started the assignment

--- Orr_jeremy_Assignment3.js
+++ Orr_jeremy_Assignment3.js
@@ -2,13 +2,19 @@
 //Assignment: Project 3
 //Class: SDI 1208
 //08/12/2012
+var afterNoon = function(mealtime){
 	if(mealtime >= 1300){
 	var afterLunch = mealTime - 12 + "PM"
+	}return afterLunch;
 }
+
 var whatMeal = function(mealTime){
+	if (mealTime >= 0 && mealTime < 3 ){
 	console.log("Let's make a midnight snack")
 	} else if(mealTime >= 3 && mealTime < 6 ){
+	console.log("Why are we making a meal at " + mealTime + ")
 	} else if(mealTime >= 6 && mealTime < 9 ){
+	console.log("Its only " + mealTime + "but let's make breakfast")
 	} else if(mealTime >= 9 && mealTime < 12 ){
 	console.log("Since it's " + mealTime + "let's make a " + snack +".")
 	} else if(mealTime >= 12 && mealTime < 15 ){
