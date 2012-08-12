05b9226e0a6c85f688ea71cb05eb97e7143450b7
Jeremy Orr <kahog@me.com>
2012-08-12 16:07:37 -0500

    added function
    started writing if statements depending on time of day

---
+++ Orr_jeremy_Assignment3.js
@@ -0,0 +1,23 @@
+//Author: Jeremy Orr
+//Assignment: Project 3
+//Class: SDI 1208
+//08/12/2012
+	if(mealtime >= 1300){
+	var afterLunch = mealTime - 12 + "PM"
+}
+var whatMeal = function(mealTime){
+	console.log("Let's make a midnight snack")
+	} else if(mealTime >= 3 && mealTime < 6 ){
+	} else if(mealTime >= 6 && mealTime < 9 ){
+	} else if(mealTime >= 9 && mealTime < 12 ){
+	console.log("Since it's " + mealTime + "let's make a " + snack +".")
+	} else if(mealTime >= 12 && mealTime < 15 ){
+	console.log("let's have lunch since it's " + afterNoon)
+	} else if(mealTime >= 15 && mealTime < 18 ){
+	console.log("")
+	} else if(mealTime >= 18 && mealTime < 21 ){
+	console.log("")
+	} else {
+	console.log("Let's make a midnight snack")
+	};
+};
