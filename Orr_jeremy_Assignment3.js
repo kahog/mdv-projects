78831e7bff3f1bdc0570097c83f0a0c3a00d3bb9
Jeremy Orr <kahog@me.com>
2012-08-12 19:55:42 -0500

    started using my JSON data to create functions and added some console.log messages
    Console.log message contain undefined var dinnerList which will be a
    list composed of what is on the JSON data of that meal with the arrays
    randomized to create the meal choice

--- Orr_jeremy_Assignment3.js
+++ Orr_jeremy_Assignment3.js
@@ -6,6 +6,12 @@
 var isHungry = true;
 	snack = ["PB and J", "Crackers and Cheese", "Corn dog"],
 	snackCount = snack[Math.floor(Math.random() * snack.length)];
+	thisMeal = function(forMeal){
+	if (mealTime() >= 3 && mealTime() < 9)
+	var forMeal = mealDetails.meals.whatsTheMeal
+	console.log(forMeal)
+	return forMeal;
+	}
 //gets the time of day being run and returns mealTime accordingly
  var mealTime =  function(){
 	var time = new Date(),
@@ -18,36 +24,39 @@ var isHungry = true;
 var hour;
 var timeOfDay;
 var whatMeal = function(mealTime){
-	if (mealTime.hour >= 0 && mealTime.hour < 3 ){
+	if (mealTime >= 0 && mealTime < 3 ){
 	console.log("Let's make a midnight snack")
 	} else if(hour >= 3 && hour < 6 ){
 	console.log("Why are we making a meal at " + mealTime())
 	} else if(mealTime >= 6 && mealTime < 9 ){
 	console.log("Its only " + mealTime + "but let's make breakfast")
+	console.log("What we are going to have for " + thisMeal() + " is " + dinnerList)
 	} else if(mealTime >= 9 && mealTime < 12 ){
 	console.log("Since it's " + mealTime + "let's make a " + snackCount +".")
 	} else if(mealTime >= 12 && mealTime < 15 ){
 	console.log("let's have lunch since it's " + timeOfDay())
+	console.log("What we are going to have for " + thisMeal() +" is " + dinnerList)
 	} else if(mealTime >= 15 && mealTime < 18 ){
 	console.log("Since it's " + timeOfDay + " let's have a " + snackCount + " and wait until dinner")
 	} else if(mealTime >= 18 && mealTime < 21 ){
 	console.log("Finally it's dinner time, I am ready to have something amazing")
+	console.log("What we are going to have for " + thisMeal() + " is " + dinnerList)
 	} else {
 	console.log("Let's make a midnight snack")
 	};
 };
-if ( isHungry = true ){
-	console.log("lets make some food I'm hungry");
-	} else {
-	console.log("I'm not hungry let's do some homework")
-	}
-whatMeal(mealTime());
+var forMeal;
 
-var hungry = function(){
-	if (isHungry = true)
-	console.log("lets make some food I'm hungry");
+var hungry = function(isHungry){
+	if (isHungry = true){
+	console.log("Let's make some food I'm hungry");
 	} else {
 	console.log("I'm not hungry let's do some homework")
-	})
-hungry();	
+	};
+};
+thisMeal(mealDetails);
+mealTime()
+console.log("Right now it is " + timeOfDay)	
+hungry(true);	
 whatMeal(mealTime());
+thisMeal(mealDetails);
--- mealdetails.js
+++ mealdetails.js
@@ -1,24 +1,24 @@
 var	mealDetails = {
 	"meals" : [
-		{"breakfast": 
+		{   whatsTheMeal: "breakfast",  
     		appetizer:   "bagel",
-    		condiments:  ["cream cheese", "ketchup", "syrup"]     
-    		entree:   	 "eggs"			
-    		meat:       ["sausage", "ham", "steak", "bacon" ]
+    		condiments:  ["cream cheese", "ketchup", "syrup"],     
+    		entree:   	 "eggs",			
+    		meat:       ["sausage", "ham", "steak", "bacon" ],
     		drink:      ["milk", "orange juice", "coffee"]
 		},
-		{"lunch": 
+		{ 	whatsTheMeal: "lunch",
     		appetizer:   "spinach and artichoke dip",
-    		condiments:  ["ketchup", "bbq sauce", "ranch dressing"]     
-    		entree:   	 "fried potatoes"			
-    		meat:       ["chicken strips", "ham", "steak"]
-    		drink:      ["Mountain Dew", "Ice Tea", "Lemonade"]
+    		condiments:  ["ketchup", "bbq sauce", "ranch dressing"],     
+    		entree:   	 "fried potatoes",			
+    		meat:       ["chicken strips", "ham", "steak"],
+    		drink:      ["Mountain Dew", "Ice Tea", "Lemonade"],
 		},
-		{"dinner": 
+		{ 	whatsTheMeal: "dinner",
     		appetizer:   "spinach and artichoke dip",
-    		condiments:  ["steak sauce", "ketchup", "salsa"]     
-    		entree:   	 "mashed potatoes"			
-    		meat:       [" baked chicken", "ham", "steak", "shrimp" ]
+    		condiments:  ["steak sauce", "ketchup", "salsa"],     
+    		entree:   	 "mashed potatoes",			
+    		meat:       [" baked chicken", "ham", "steak", "shrimp" ],
     		drink:      ["Dr. Pepper", "Ice tea", "Wine"]
     	}
     ]
