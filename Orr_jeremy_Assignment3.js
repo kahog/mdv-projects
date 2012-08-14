0734d1c3aa0a33e5b365852d14c722aa813089d8
Jeremy Orr <kahog@me.com>
2012-08-14 00:43:01 -0500

    Made changes to the syntax so it would run without errors

--- Orr_jeremy_Assignment3.js
+++ Orr_jeremy_Assignment3.js
@@ -6,7 +6,9 @@
 var isHungry = true;
 	snack = ["PB and J", "Crackers and Cheese", "Corn dog"],
 	snackCount = snack[Math.floor(Math.random() * snack.length)];
-	thisMeal = function(forMeal){
+	
+//procedure function	
+var	thisMeal = function(forMeal){
 	if (mealTime() >= 3 && mealTime() < 9){
 	var forMeal = mealDetails["meals"]["breakfast"].TheMealIs;
 	console.log(forMeal)
@@ -17,8 +19,9 @@ var isHungry = true;
 	} else {
 	var forMeal = mealDetails["meals"]["dinner"].TheMealIs;
 	console.log(forMeal)
-	}
+	} return forMeal;
 };
+
 //gets the time of day being run and returns mealTime accordingly
  var mealTime =  function(){
 	var time = new Date(),
@@ -30,6 +33,8 @@ var isHungry = true;
 };
 var hour;
 var timeOfDay;
+
+
 //decide what meal we are having and runs console.log statements determining what we are having
 var whatMeal = function(mealTime){
 	if (mealTime >= 0 && mealTime < 3 ){
@@ -38,17 +43,17 @@ var whatMeal = function(mealTime){
 	console.log("Why are we making a meal at " + mealTime())
 	} else if(mealTime >= 6 && mealTime < 9 ){
 	console.log("Its only " + mealTime + "but let's make breakfast")
-	console.log("What we are going to have for " + thisMeal() + " is " + dinnerList)
+	console.log(randomMealChoice(forMeal))
 	} else if(mealTime >= 9 && mealTime < 12 ){
 	console.log("Since it's " + mealTime + "let's make a " + snackCount +".")
 	} else if(mealTime >= 12 && mealTime < 15 ){
-	console.log("let's have lunch since it's " + timeOfDay())
-	console.log("What we are going to have for " + thisMeal(forMeal) +" is " + dinnerList)
+	console.log("let's have lunch since it's " + timeOfDay)
+//	console.log(randomMealChoice(forMeal))
 	} else if(mealTime >= 15 && mealTime < 18 ){
 	console.log("Since it's " + timeOfDay + " let's have a " + snackCount + " and wait until dinner")
 	} else if(mealTime >= 18 && mealTime < 21 ){
 	console.log("Finally it's dinner time, I am ready to have something amazing")
-	console.log("What we are going to have for " + thisMeal(forMeal) + " is " + dinnerList)
+	console.log(randomMealChoice(forMeal))
 	} else {
 	console.log("Let's make a midnight snack")
 	};
@@ -63,12 +68,34 @@ var hungry = function(isHungry){
 	};
 };
 //number function
-var mealChoices = function(meal){};
+//Number of available meal choices throughout the day
+
+var mealChoices = function(forMeal){
+	var numberMealChoices = mealDetails["meals"][forMeal].condiments.length * 
+	mealDetails["meals"][forMeal].meat.length *mealDetails["meals"][forMeal].drink.length;
+	console.log("The number of meal choices we have for " + forMeal + " is " + numberMealChoices + ".");
+ }; 
+ 
+ //string concatenation
+	var drinkChoice		= mealDetails["meals"][forMeal].drink
+		meatChoice		= mealDetails["meals"][forMeal].meat		
+		condimentChoice	= mealDetails["meals"][forMeal].condiments
+	
+		randomCondimentChoice 	= condimentChoice[Math.floor(Math.random() * condimentChoice.length)]
+	   	randomMeatChoice 		= meatChoice[Math.floor(Math.random() * meatChoice.length)]
+		randomDrinkChoice		= drinkChoice[Math.floor(Math.random() * drinkChoice.length)]
+		chosenChoices	= console.log("For " +forMeal + " we are going to have " + mealDetails["meals"][forMeal].appetizer +", " + mealDetails["meals"][forMeal].entree + ", " + randomDrinkChoice + ", " + randomMeatChoice+ ", and " + randomCondimentChoice+ ".")
+		return chosenChoices;
+	};
+
+
+
 mealTime()
 console.log("Right now it is " + timeOfDay)	
 hungry(true);	
 whatMeal(mealTime());
-thisMeal(forMeal);
+mealChoices(thisMeal());
+randomMealChoice(thisMeal(forMeal));
 
 
 
